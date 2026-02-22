8895d98cc1bb83f25bbf2b96ebee5ffb0cc7fe8a
Valinee <117422015+Dazuke@users.noreply.github.com>
2026-02-22 13:12:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,10 @@ class Particle{
 constructor(){
 this.x = Math.random()*canvas.width;
 this.y = Math.random()*canvas.height;
-this.size = Math.random()*2;
-this.speedY = Math.random()*0.5+0.2;
+this.size = Math.random()*2+1;
+this.speedY = Math.random()*1+0.5;
 }
+
 update(){
 this.y -= this.speedY;
 if(this.y < 0){
@@ -25,8 +26,9 @@ this.y = canvas.height;
 this.x = Math.random()*canvas.width;
 }
 }
+
 draw(){
-ctx.fillStyle = "rgba(255,182,193,0.7)";
+ctx.fillStyle="rgba(255,255,255,0.5)";
 ctx.beginPath();
 ctx.arc(this.x,this.y,this.size,0,Math.PI*2);
 ctx.fill();
@@ -34,24 +36,19 @@ ctx.fill();
 }
 
 function init(){
-for(let i=0;i<120;i++){
+for(let i=0;i<100;i++){
 particlesArray.push(new Particle());
 }
 }
 
 function animate(){
 ctx.clearRect(0,0,canvas.width,canvas.height);
-particlesArray.forEach(p=>{
-p.update();
-p.draw();
-});
+for(let i=0;i<particlesArray.length;i++){
+particlesArray[i].update();
+particlesArray[i].draw();
+}
 requestAnimationFrame(animate);
 }
 
 init();
 animate();
-
-window.addEventListener("resize",()=>{
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-});
